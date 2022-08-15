193f11739cbf78c1b028ece9f561e7c4fba0aad1
Mahreen-Asama <mahreenmehar202@gmail.com>
2022-08-15 21:50:49 +0500

    amenities start

---
+++ ApnaGharV2/Models/Property.cs
@@ -0,0 +1,33 @@
+﻿namespace ApnaGharV2.Models
+{
+    public class Property
+    {
+        public int Id { get; set; }
+
+        public string Purpose { get; set; } //sale, rent
+
+        public string Type { get; set; } //house, plot, commercial
+
+        public string SubType { get; set; } //from each type there is a sub type
+
+        public string City { get; set; } //in which city this property reside
+
+        public string Area { get; set; } //where property is located
+        public string Address { get; set; } //full address
+        public string Bedrooms { get; set; } //full address
+        public string Bathrooms { get; set; } //full address
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Price { get; set; }   //save as 45 Lakh
+
+        public string Size { get; set; }    //save as 12 Sqft
+
+        public string Image { get; set; }
+
+        public int OwnerId { get; set; }
+
+    }
+}
---
+++ ApnaGharV2/Models/PropertyAmenities.cs
@@ -0,0 +1,13 @@
+﻿namespace ApnaGharV2.Models
+{
+    public class PropertyAmenities
+    {
+        public string MainFeatures { get; set; }
+        public string BusinessAndCommunication { get; set; }
+        public string NearbyLocations { get; set; }
+        public string Rooms { get; set; }
+        public string HealthcareRecreational { get; set; }
+        public string OtherFacilities { get; set; }
+
+    }
+}
